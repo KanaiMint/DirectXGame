cb09cc42cc7c0fb5fb8a3edfc1c66495b8ccb6d4
mint <k022g0015@m.neec.ac.jp>
2023-04-16 16:39:46 +0900

    プロジェクト ファイルを追加します。

---
+++ math/Vector3.h
@@ -0,0 +1,29 @@
+#pragma once
+#include<cmath>
+
+
+/// <summary>
+/// 3次元ベクトル
+/// </summary>
+struct Vector3 final {
+	float x;
+	float y;
+	float z;
+
+	//加算
+	static Vector3 Add(const Vector3& v1, const Vector3& v2);
+	//減算
+	static Vector3 Subtract(const Vector3& v1, const Vector3& v2);
+	//スカラー算
+	static Vector3 Multiply(float scalar, const Vector3& v);
+	//内積
+	static float Dot(const Vector3& v1, const Vector3& v2);
+	//長さ(ノルム)
+	static float Length(const Vector3& v);
+	//正規化
+	static Vector3 Normalize(const Vector3& v);
+};
+
+static const int kColumWidth = 60;
+
+ void VecScreenPrintf(int x, int y, const Vector3& vector, const char* label);
